23bcfdb39a86229c6124d117df95fe49d29932fb
xiaoyuntao <yuntao@xbongbong.com>
2025-03-14 22:48:29 +0800

    init

---
+++ spring-ai-alibaba-scene-example/multimodelchat/src/main/java/com/alibaba/example/multimodelchat/MultiModelChatApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.example.multimodelchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModelChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModelChatApplication.class, args);
+    }
+
+}
