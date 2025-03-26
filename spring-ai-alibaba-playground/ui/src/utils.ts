e6d664524545ff5d335f432ca11460190a441349
yuluo-yx <yuluo08290126@gmail.com>
2025-03-26 09:07:14 +0800

    feat
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

---
+++ spring-ai-alibaba-playground/ui/src/utils.ts
@@ -0,0 +1,13 @@
+import { message } from "antd";
+import type { GetProp, UploadProps } from "antd";
+
+type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
+
+// 限制文件大小
+export const litFileSize = (file: FileType, size: number) => {
+  const isLt2M = file.size / 1024 / size < 1;
+  if (!isLt2M) {
+    message.error(`Image must smaller than ${size}KB!`);
+  }
+  return isLt2M;
+};
