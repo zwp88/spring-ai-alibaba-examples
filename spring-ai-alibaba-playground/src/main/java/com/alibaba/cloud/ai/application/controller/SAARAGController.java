3218ea7a785ad49d9183238218cd0eb72eb2d713
yuluo-yx <yuluo08290126@gmail.com>
2025-04-12 12:23:48 +0800

    feat: 更新 readme 文件添加对 RAG 的说明ï
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- spring-ai-alibaba-playground/src/main/java/com/alibaba/cloud/ai/application/controller/SAARAGController.java
+++ spring-ai-alibaba-playground/src/main/java/com/alibaba/cloud/ai/application/controller/SAARAGController.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.cloud.ai.application.controller;
 
 import com.alibaba.cloud.ai.application.annotation.UserIp;
@@ -5,6 +22,7 @@ import com.alibaba.cloud.ai.application.service.SAARAGService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
+import reactor.core.publisher.Flux;
 
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +50,7 @@ public class SAARAGController {
 	@UserIp
 	@GetMapping("/rag")
 	@Operation(summary = "DashScope RAG")
-	public String ragChat(
+	public Flux<String> ragChat(
 			HttpServletResponse response,
 			@Validated @RequestParam("prompt") String prompt,
 			@RequestHeader(value = "chatId", required = false, defaultValue = "spring-ai-alibaba-playground-rag") String chatId
