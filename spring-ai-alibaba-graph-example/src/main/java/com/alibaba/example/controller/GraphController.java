0fa4933297a300f2b27032912790b7e165b1d88b
HY-love-sleep <583699747@qq.com>
2025-06-04 10:05:09 +0800

    chore: add license and style code

--- spring-ai-alibaba-graph-example/src/main/java/com/alibaba/example/controller/GraphController.java
+++ spring-ai-alibaba-graph-example/src/main/java/com/alibaba/example/controller/GraphController.java
@@ -1,15 +1,27 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.example.controller;
 
 import com.alibaba.cloud.ai.graph.CompiledGraph;
-import com.alibaba.cloud.ai.graph.OverAllState;
 import com.alibaba.cloud.ai.graph.exception.GraphStateException;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/graph")
@@ -25,7 +37,7 @@ public class GraphController {
     public ResponseEntity<Map<String, Object>> invoke(
             @RequestBody Map<String, Object> inputs) throws GraphStateException {
 
-        // 执行图
+        // invoke graph
         var resultFuture = graph.invoke(inputs);
 
         return ResponseEntity.ok(resultFuture.get().data());
--- spring-ai-alibaba-graph-example/src/main/java/com/alibaba/example/graph/conf/ComplexSupportGraphBuilder.java
+++ spring-ai-alibaba-graph-example/src/main/java/com/alibaba/example/graph/conf/ComplexSupportGraphBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.example.graph.conf;
 
 import com.alibaba.cloud.ai.graph.CompiledGraph;
@@ -14,8 +30,6 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.tool.resolution.ToolCallbackResolver;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.http.HttpMethod;
