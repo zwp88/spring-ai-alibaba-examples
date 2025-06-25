76fcbeff0882f43326b1bef572fe4132cd1cae81
yingzi <GTyingzi@gmail.com>
2025-06-26 02:11:25 +0800

    add mcp、human

---
+++ spring-ai-alibaba-graph-example/human-node/src/main/java/com/alibaba/cloud/ai/graph/HumanNodeApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.cloud.ai.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yingzi
+ * @since 2025/6/13
+ */
+@SpringBootApplication
+public class HumanNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanNodeApplication.class, args);
+    }
+}
---
+++ spring-ai-alibaba-graph-example/mcp-node/src/main/java/com/alibaba/cloud/ai/graph/McpNodeApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.cloud.ai.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yingzi
+ * @since 2025/6/13
+ */
+@SpringBootApplication
+public class McpNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpNodeApplication.class, args);
+    }
+}
