fd7070fcec8448976068ea17dc9cd0790bd7d9a0
yingzi <GTyingzi@gmail.com>
2025-06-29 01:03:02 +0800

    add mcp-auth exmaple

---
+++ spring-ai-alibaba-mcp-example/spring-ai-alibaba-mcp-auth-example/server/mcp-auth-server/src/main/java/com/alibaba/cloud/ai/mcp/server/model/RestfulModel.java
@@ -0,0 +1,12 @@
+package com.alibaba.cloud.ai.mcp.server.model;
+
+import org.springframework.http.HttpMethod;
+
+/**
+ * @author yingzi
+ * @since 2025/6/28
+ */
+
+public record RestfulModel(String name, String description, String inputSchema, String url, String method, String path, HttpMethod httpMethod) {
+
+}
